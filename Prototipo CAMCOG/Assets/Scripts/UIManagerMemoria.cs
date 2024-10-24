21e3beee4b5c52e068bb5f269bad99dc359db88c
DiegoAlonsoValenciano <125390412+DiegoAlonsoValenciano@users.noreply.github.com>
2024-10-24 15:33:07 +0200

    memoria1.0

--- Prototipo CAMCOG/Assets/Scripts/UIManagerMemoria.cs
+++ Prototipo CAMCOG/Assets/Scripts/UIManagerMemoria.cs
@@ -1,18 +1,89 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System.Collections.Generic;
+using System.IO;
 
 public class UIManagerMemoria : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Image imageUI;
+    public Image imageUI2;
+    public Image imageUI3;
+    private List<string> imagePaths;
+    private int currentIndex = 0;
+
+    [System.Serializable]
+    public class ImageData
     {
-        
+        public List<string> images;
     }
 
-    // Update is called once per frame
-    void Update()
+    void Start()
     {
-        
+        LoadJson();
+        LoadNextImage();
     }
+
+    void LoadJson()
+    {
+        TextAsset jsonFile = Resources.Load<TextAsset>("ConfigEntradaMemoria");
+
+        if (jsonFile != null)
+        {
+            ImageData jsonData = JsonUtility.FromJson<ImageData>(jsonFile.text);
+            imagePaths = jsonData.images;
+            Debug.Log("LoadJson");
+        }
+        else
+        {
+            Debug.LogError("No se pudo encontrar el archivo JSON.");
+        }
+    }
+
+    public void LoadNextImage()
+    {
+        Debug.Log("LoadNextImage");
+        Debug.Log(imagePaths.Count);
+        if (imagePaths != null && imagePaths.Count > 0)
+        {
+            Debug.Log("LoadNextImage2");
+            string imagePath = imagePaths[currentIndex];
+            string imagePath2 = imagePaths[currentIndex];
+            string imagePath3 = imagePaths[currentIndex];
+            Sprite sprite = Resources.Load<Sprite>(imagePath);
+            Sprite sprite2 = Resources.Load<Sprite>(imagePath2);
+            Sprite sprite3 = Resources.Load<Sprite>(imagePath3);
+            Debug.Log("LoadNextImage3");
+            if (sprite != null)
+            {
+                imageUI.sprite = sprite;
+                Debug.Log("image1");
+            }
+            else
+            {
+                Debug.LogError("No se pudo cargar la imagen: " + imagePath);
+            }
+            if (sprite2 != null)
+            {
+                imageUI2.sprite = sprite2;
+                Debug.Log("image2");
+            }
+            else
+            {
+                Debug.LogError("No se pudo cargar la imagen: " + imagePath2);
+            }
+            if (sprite3 != null)
+            {
+                imageUI3.sprite = sprite3;
+                Debug.Log("image3");
+            }
+            else
+            {
+                Debug.LogError("No se pudo cargar la imagen: " + imagePath3);
+            }
+
+            currentIndex = (currentIndex + 1) % imagePaths.Count;
+        }
+    }
+    // Start is called before the first frame update
 }
