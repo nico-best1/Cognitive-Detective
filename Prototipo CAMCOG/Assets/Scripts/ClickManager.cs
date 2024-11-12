132c458eb2bceaa81802eb274ed5ff0af097dedc
Nicolás Besteiro <nicobest@ucm.es>
2024-11-12 19:26:55 +0100

    La puesrta 2 va a comisaria(de momento al almacen)
    mañana por la mañana sigo ajaja

--- Prototipo CAMCOG/Assets/Scripts/ClickManager.cs
+++ Prototipo CAMCOG/Assets/Scripts/ClickManager.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.SearchService;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ClickManager : MonoBehaviour
 {
@@ -9,6 +11,10 @@ public class ClickManager : MonoBehaviour
     {
         Debug.Log("entra");
         GameManager.Instance.setBackground(obj.nNext, obj.nActual);
+    }
+    public void GoToAlmacenPruebas()
+    {
+        GameManager.Instance.ChangeScene(2);
     }
     public void destroyObject(GameObject obj)
     {
