ac355047faca1bd2bb73b2dc11a2190fee1f6f0e
Adriaan63 <Adcast07@ucm.es>
2024-11-26 12:26:26 +0100

    implementando textos

---
+++ Prototipo CAMCOG/Assets/Scripts/Dialogues/ActivateMessages.cs
@@ -0,0 +1,42 @@
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+
+//public class ActivateMessages : MonoBehaviour
+//{
+//    private GameObject _player;
+//    public Textos text;
+//    [SerializeField]private DialogueControl _dialogueControl;
+//    [SerializeField] private GameObject _message;
+
+//    private void OnTriggerEnter2D(Collider2D collision)
+//    {
+//        if ((bool)collision.gameObject.GetComponent<MovementComponent>())
+//        {
+//             _player.GetComponent<InputComponent>().enabled = false;
+//            _player.GetComponent<MovementComponent>().Move(0);
+//            _dialogueControl.MessageActive(text);
+            
+//        }
+//    }
+//    private void OnTriggerExit2D(Collider2D collision)
+//    {
+//        if ((bool)collision.gameObject.GetComponent<MovementComponent>())
+//        {
+//            _message.SetActive(false);
+
+//        }
+//    }
+
+//    // Start is called before the first frame update
+//    void Start()
+//    {
+//        _player = GameManager.Instance.SetPlayer();
+//    }
+
+//    // Update is called once per frame
+//    void Update()
+//    {
+        
+//    }
+//}
---
+++ Prototipo CAMCOG/Assets/Scripts/Dialogues/DialogueControl.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueControl : MonoBehaviour
+{
+    private Queue <string> _colaDialogos;  
+    Textos _text;
+    [SerializeField] TextMeshProUGUI _textMeshPro;
+    [SerializeField] private float _timeCaracter;
+    private Animator _animator;
+    private GameObject _player;
+    [SerializeField] private Button _button;
+    
+    public void MessageActive(Textos ObjectText)
+    {
+        //animacion cartel
+        Debug.Log("Entro");
+        _animator.SetBool("Activado", true);
+        _text = ObjectText;
+
+    }
+    public void TextActive()
+    {
+        _colaDialogos.Clear();
+        foreach(string _saveText in _text._arrayText)
+        {
+            _colaDialogos.Enqueue(_saveText);
+        }
+        NextPhrase();
+    }
+    public void NextPhrase()
+    {
+        if(_colaDialogos.Count == 0)
+        {
+            CloseMessage();
+            _textMeshPro.text = "";
+            return;
+        }
+        string _targetPhrase = _colaDialogos.Dequeue();
+        _textMeshPro.text = _targetPhrase;
+        StartCoroutine(ShowCharacters(_targetPhrase));
+
+    }
+    IEnumerator ShowCharacters(string showText)
+    {
+        _textMeshPro.text = "";
+        foreach(char caracter in showText.ToCharArray())
+        {
+            _button.interactable = false;
+            _textMeshPro.text += caracter;
+            yield return new WaitForSeconds(_timeCaracter);
+        }
+        _button.interactable = true;
+
+    }
+
+    public void CloseMessage()
+    {
+        //_player.GetComponent<InputComponent>().enabled= true;
+        _animator.SetBool("Activado", false);
+        _colaDialogos.Clear();
+        
+    }
+    public void ResetAnim()
+    {
+        _animator.SetTrigger("Empezar");
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+        _colaDialogos = new Queue<string>();
+        //_player = GameManager.Instance.SetPlayer();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
