dd60aa1c423237eff01cc4a7d73b35c421bf4652
Adriaan63 <Adcast07@ucm.es>
2024-12-05 15:27:46 +0100

    libro inicial con cambios de testos

--- Prototipo CAMCOG/Assets/Scripts/Dialogues/DialogueControl.cs
+++ Prototipo CAMCOG/Assets/Scripts/Dialogues/DialogueControl.cs
@@ -6,20 +6,24 @@ using UnityEngine.UI;
 
 public class DialogueControl : MonoBehaviour
 {
-    private Queue <string> _colaDialogos;  
-    Textos _text;
-    [SerializeField] TextMeshProUGUI _textMeshPro;
+    private Queue<string> _colaDialogos;
+    private Textos _text;
+    [SerializeField] private List<TextMeshProUGUI> _textFields; // Lista de TextMeshProUGUI
     [SerializeField] private float _timeCaracter;
     private Animator _animator;
     private GameObject _player;
     [SerializeField] private Button _button;
     [SerializeField] private GameObject texto1;
+    private int _currentTextFieldIndex = 0; // Índice del TextMeshPro actual
+
     [System.Serializable]
     public class ImageData
     {
         public List<string> images;
     }
+
     private List<Textos> imagePaths;
+
     [System.Serializable]
     public class GenericJsonData
     {
@@ -35,92 +39,108 @@ public class DialogueControl : MonoBehaviour
 
     public void MessageActive(Textos ObjectText)
     {
-        //animacion cartel
+        // Animación de cartel
         Debug.Log("Entro");
         _animator.SetBool("Activado", true);
         _text = ObjectText;
-
     }
+
     public Textos selectText(int i)
     {
         Debug.Log(imagePaths);
         return imagePaths[i];
     }
+
     public void TextActive()
     {
         _colaDialogos.Clear();
-        foreach(string _saveText in _text._arrayText)
+        foreach (string _saveText in _text._arrayText)
         {
             _colaDialogos.Enqueue(_saveText);
         }
+        _currentTextFieldIndex = 0; // Reinicia al primer campo
         NextPhrase();
     }
+
     public void NextPhrase()
     {
-        if(_colaDialogos.Count == 0)
+        if (_colaDialogos.Count == 0)
         {
             CloseMessage();
-            _textMeshPro.text = "";
+            ClearAllTextFields();
             return;
         }
-        string _targetPhrase = _colaDialogos.Dequeue();
-        _textMeshPro.text = _targetPhrase;
-        StartCoroutine(ShowCharacters(_targetPhrase));
 
+        string _targetPhrase = _colaDialogos.Dequeue();
+        StartCoroutine(ShowCharacters(_targetPhrase));
     }
+
     IEnumerator ShowCharacters(string showText)
     {
-        _textMeshPro.text = "";
-        foreach(char caracter in showText.ToCharArray())
+        _textFields[_currentTextFieldIndex].text = "";
+        Debug.Log(showText);
+        int char_index = 0;
+        string auxText = showText;
+        // Asegurarse de que haya un campo disponible
+        while (_currentTextFieldIndex < _textFields.Count && char_index < auxText.Length)
         {
+            var currentField = _textFields[_currentTextFieldIndex];
             _button.interactable = false;
-            _textMeshPro.text += caracter;
-            yield return new WaitForSeconds(_timeCaracter);
-        }
-        _button.interactable = true;
 
+            foreach (char caracter in showText.ToCharArray())
+            {
+
+                if (IsTextFieldFull(currentField))
+                {
+                    _currentTextFieldIndex++;
+                    if(char_index < showText.Length)
+                        showText = showText.Substring(char_index);
+                    break; // Pasa al siguiente TextMeshProUGUI
+                }
+                currentField.text += caracter;
+                char_index++;
+                yield return new WaitForSeconds(_timeCaracter);
+            }
+            _button.interactable = true;
+        }
+
+        if (_currentTextFieldIndex >= _textFields.Count)
+        {
+            Debug.LogWarning("No hay más campos disponibles para mostrar texto.");
+            _button.interactable = true;
+        }
+    }
+
+    private bool IsTextFieldFull(TextMeshProUGUI textField)
+    {
+        // Verificar si la altura requerida por el texto excede la altura del campo
+        return textField.preferredHeight > textField.rectTransform.rect.height;
+    }
+
+    private void ClearAllTextFields()
+    {
+        foreach (var field in _textFields)
+        {
+            field.text = "";
+        }
     }
 
     public void CloseMessage()
     {
-        //_player.GetComponent<InputComponent>().enabled= true;
         _animator.SetBool("Activado", false);
         _colaDialogos.Clear();
-        
     }
+
     public void ResetAnim()
     {
         _animator.SetTrigger("Empezar");
     }
+
     public void PassScene()
     {
         GameManager.Instance.ChangeScene(1);
     }
-    //public void LoadJson()
-    //{
-    //    TextAsset jsonFile = Resources.Load<TextAsset>("ConfigEntradaReconocimiento");
-    //    imagePaths = new List<Textos>();
-    //    Debug.Log(jsonFile);
-    //    if (jsonFile != null)
-    //    {
-    //        //for (int i = 0; i < jsonFile.text.Length; i++)
-    //        //{
-    //            ImageData jsonData = JsonUtility.FromJson<ImageData>(jsonFile.text);
-    //            Textos text = new Textos();
-    //            text._arrayText = jsonData.images;
-    //            for (int i = 0; i < text._arrayText.Count; i++)
-    //            {
-    //                Debug.Log(text._arrayText[i]);
-    //            }
-    //            imagePaths.Add(text);
 
-    //        //}
-    //    }
-    //    else
-    //    {
-    //        Debug.LogError("No se pudo encontrar el archivo JSON.");
-    //    }
-    //}
     public void LoadJson()
     {
         TextAsset jsonFile = Resources.Load<TextAsset>("ConfigEntradaReconocimiento");
@@ -144,20 +164,15 @@ public class DialogueControl : MonoBehaviour
         }
     }
 
-    // Start is called before the first frame update
     void Start()
     {
         LoadJson();
         _animator = GetComponent<Animator>();
         _colaDialogos = new Queue<string>();
         texto1.GetComponent<ActivateMessages>().ActivateText();
-    
-    //_player = GameManager.Instance.SetPlayer();
-}
+    }
 
-    // Update is called once per frame
     void Update()
     {
-        
     }
 }
