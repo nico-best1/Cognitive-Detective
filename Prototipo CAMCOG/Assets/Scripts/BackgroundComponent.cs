e0540a2ea1cdacba3f34426e8c959d0333a14ecb
Adriaan63 <Adcast07@ucm.es>
2024-12-05 14:27:26 +0100

    cambios

--- Prototipo CAMCOG/Assets/Scripts/BackgroundComponent.cs
+++ Prototipo CAMCOG/Assets/Scripts/BackgroundComponent.cs
@@ -5,6 +5,32 @@ using UnityEngine;
 public class BackgroundComponent : MonoBehaviour
 {
     private Animator Animator;
+    [SerializeField] int nextBack;
+    [SerializeField] int actBack;
+    public void OnAnimationEnd()
+    {
+        if (GameManager.Instance != null)
+        {
+            switch (GameManager.Instance.opcionMenu)
+            {
+                case 0:
+                    GameManager.Instance.setBackground(nextBack, actBack, "");
+                    break;
+                case 1:
+                    GameManager.Instance.changeSceneMenu(1);
+                    break;
+                case 2:
+                    Debug.Log("Salir Juego");
+                    GameManager.Instance.CloseApp();
+                    break;
+            }
+
+        }
+        else
+        {
+            Debug.LogError("No se encontró una instancia de UIManagerGame.");
+        }
+    }
     private void Start()
     {
         Animator = GetComponent<Animator>();
